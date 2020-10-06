b36f3d1de21f9589150ed94e51864c7797a5381c
Franco Ezequiel Marchegiani <eze.cavs@hotmail.com>
2020-10-06 11:17:54 -0300

    Quinto intento de subir repositorio xd

---
+++ js/darkMode.js
@@ -0,0 +1,22 @@
+const btnSwitch = document.querySelector('#switch');
+
+btnSwitch.addEventListener('click', () => {
+    document.body.classList.toggle('dark');
+    btnSwitch.classList.toggle('active');
+
+    // Guarda el modo en el localStorage.
+    if(document.body.classList.contains('dark')){
+        localStorage.setItem('dark-mode', 'true');
+    } else {
+        localStorage.setItem('dark-mode', 'false');
+    }
+});
+
+// Modo actual en el que estamos
+if(localStorage.getItem('dark-mode') === 'true'){
+    document.body.classList.add('dark');
+    btnSwitch.classList.add('active');
+} else{
+    document.body.classList.remove('dark');
+    btnSwitch.classList.remove('active');
+}
